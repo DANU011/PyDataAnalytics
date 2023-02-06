08d1af49a999d15dca13e7336639d3c7a190afc9
DANU011 <122770856+DANU011@users.noreply.github.com>
2023-02-07 00:24:07 +0900

    Add files via upload

---
+++ src/06-2/Galm.js
@@ -0,0 +1,23 @@
+import Galc1 from "./Galc1";
+import Galcard from "./Galcard";
+import { useState, useEffect } from "react";
+
+const Galm = ({galt, datas}) => {
+  let [selgalt, setSelgalt] = useState();
+  let [selcard, setSelcard] = useState([]);
+  
+  useEffect (() => {
+    if (selTitle) {
+      setSelcard(datas.filter((i) => i["galTitle"] === selgalt)[0])
+    }
+  }, [selgalt]);
+  
+  return (
+    <div className="galm">
+      <Galc1 galt={galt} selgalt={selgalt} setSelgalt={setSelgalt}/>
+      <Galcard selcard={selcard}/>
+    </div>
+  );
+}
+
+export default Galm;
