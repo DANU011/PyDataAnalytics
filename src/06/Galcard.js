92e3137a10404f2e2f753489caca8456db6cc210
DANU011 <122770856+DANU011@users.noreply.github.com>
2023-02-07 10:28:03 +0900

    Update Galcard.js

--- src/06/Galcard.js
+++ src/06/Galcard.js
@@ -5,10 +5,10 @@ const Galcard = (probs) => {
                <div className="images"><img src={probs.selcard["galWebImageUrl"] } className="img"/>
                </div>
                <div className="gcdtext">
-               <div className="galTitle">{probs.selcard["galTitle"]}</div>
-               <div className="galPhotographyLocation">{probs.selcard["galPhotographyLocation"]}</div>
-               <div className="galPhotographer">{probs.selcard["galPhotographer"]}</div>
-               <div className="galSearchKeyword">{probs.selcard["galSearchKeyword"]}</div>
+               <div>{probs.selcard["galTitle"]}</div>
+               <div>{probs.selcard["galPhotographyLocation"]}</div>
+               <div>{probs.selcard["galPhotographer"]}</div>
+               <div>{probs.selcard["galSearchKeyword"]}</div>
                </div>
           </div>
      );
