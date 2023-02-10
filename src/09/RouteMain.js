7f98460083ac95a5416734a46cfc46a339de8798
DANU011 <122770856+DANU011@users.noreply.github.com>
2023-02-10 13:52:50 +0900

    Add files via upload

--- src/09/RouteMain.js
+++ src/09/RouteMain.js
@@ -5,16 +5,21 @@ import RouterPage2 from "./RouterPage2";
 
 import { Route,Routes } from "react-router-dom";
 
+
+
+
+
 const RouteMain = ()=>{
+      
      return(
           <>
           <RouterNv />
           <Routes>
                <Route path="/" element={<RouterHome />} />
-               <Route path="/p1" element={<RouterPage1 />} />
+               <Route path="/p1/:item" element={<RouterPage1 />} />
                <Route path="/p2" element={<RouterPage2 />} />
           </Routes>
           </>
-     );
+     ); //<Route path="/p1/:item" /:item 아이템을 달고 가겠다. 변수 다른거 써도됨.
 }
 export default RouteMain;
